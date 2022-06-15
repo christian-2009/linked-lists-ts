88af730f6f83b3ad81a6dc2d7dfff4953bdf0e30
christian-2009 <c-sophocleous@hotmail.com>
2022-06-15 10:11:41 +0100

    comnpleted print linked list

---
+++ src/printLinkedList.test.ts
@@ -0,0 +1,6 @@
+
+import {exampleHead, printLinkedList} from './printLinkedList'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(printLinkedList(exampleHead)).toBe(undefined);
+});
---
+++ src/printLinkedList.ts
@@ -0,0 +1,21 @@
+interface LLNode {
+	val: number;
+	next: LLNode | null;
+}
+
+const C: LLNode = {val: 3, next: null}
+const B: LLNode = {val: 2, next: C}
+const A: LLNode = {val : 1, next : B }
+export const exampleHead: LLNode = A
+
+
+export function printLinkedList(head: LLNode | null):void{
+
+	let current = head
+	while(current){
+		console.log(current.val)
+		current = current.next
+	}
+}
+
+printLinkedList(exampleHead)
